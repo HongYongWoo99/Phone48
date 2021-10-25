6a011ef94e64342844de8e0f7212e3840c4e3afd
kanghyuntae <eilc3397@gmail.com>
2021-10-25 20:04:16 +0900

    강현태, 이찬희, 최재일 취합본

---
+++ Phone48/src/main/java/org/phone48/service/BoardService.java
@@ -0,0 +1,91 @@
+package org.phone48.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.phone48.dto.BoardDTO;
+import org.phone48.dto.Criteria;
+import org.phone48.dto.FileDTO;
+import org.phone48.dto.ReviewDTO;
+import org.phone48.mapper.BoardMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+	private BoardMapper mapper;
+	//---------------------------KANG
+	public BoardService(BoardMapper mapper) {
+		super();
+		this.mapper = mapper;
+	}
+
+	public int selectBoardCount() {
+		return mapper.selectBoardCount();
+	}
+
+	public int insertBoard(BoardDTO dto) {
+		int bno = mapper.selectBoardNo();
+		dto.setBno(bno);
+		mapper.insertBoard(dto);
+		return bno;
+	}
+	
+	public void insertFileList(ArrayList<FileDTO> flist) {
+		for(int i=0;i<flist.size();i++) {
+			mapper.insertFileInfo(flist.get(i));
+		}
+	}
+	
+	public void deleteBoard(int bno) {
+		mapper.deleteBoard(bno);
+	}
+	
+	public BoardDTO selectBoardContent(int bno) {
+		return mapper.selectBoardContent(bno);
+	}
+	
+	public void updateBoard(int bno, String title, String content) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("bno", bno);
+		map.put("title", title);
+		map.put("content", content);
+		mapper.updateBoard(map);		
+	}
+
+	public ArrayList<BoardDTO> selectBoard(int pageNo) {
+		return mapper.selectBoard(pageNo);
+	}
+
+	public void addBoardCount(int bno) {
+		mapper.addBoardCount(bno);
+	}
+
+	public ArrayList<FileDTO> selectFileList(int bno) {
+		return mapper.selectFileList(bno);
+	}
+	
+	//---------------------------KANG
+	
+//	// 게시글 목록 --CHOI
+//    public List<ReviewDTO> reviewListService() throws Exception{
+//        return mapper.reviewList();
+//    }
+//    // 게시글 상세 --CHOI
+//    public ReviewDTO reviewDetailService(int rno) throws Exception {
+//    	 return mapper.reviewDetail(rno);
+//    }
+//    //게시글 작성 --CHOI
+//    public int reviewInsertService(ReviewDTO board) throws Exception{
+//        return mapper.reviewInsert(board);
+//    }
+//    // 페이징 --CHOI
+//    public int boardListCnt() throws Exception {
+//        return mapper.boardListCnt();
+//    }
+//    // 페이징 --CHOI
+//    public List<Map<String, Object>> boardList(Criteria cri) throws Exception {
+//        return mapper.boardList(cri);
+//    }
+}
---
+++ Phone48/src/test/java/org/phone48/Phone48ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.phone48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phone48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
